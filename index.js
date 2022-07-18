fe19a38dd70f74781a5f988818f6edb79094f8ee
Inieke Sendy <ini.e.sendy@gmail.com>
2022-07-18 16:08:26 +0100

    Update: Linter Errors

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const HamburgerMenu = document.querySelector('.hamburger-menu')
-const MobileNav = document.querySelector('.mobile-nav-links')
-const MobileNavLists = document.querySelectorAll('.mobile-links')
+const HamburgerMenu = document.querySelector('.hamburger-menu');
+const MobileNav = document.querySelector('.mobile-nav-links');
+const MobileNavLists = document.querySelectorAll('.mobile-links');
 
-HamburgerMenu.addEventListener('click', function(){
-    HamburgerMenu.classList.toggle('is-active')
-    MobileNav.classList.toggle('is-active')
-})
+HamburgerMenu.addEventListener('click', () => {
+  HamburgerMenu.classList.toggle('is-active');
+  MobileNav.classList.toggle('is-active');
+});
 
-MobileNavLists.forEach(link=>{
-    link.addEventListener('click', function(){
-     HamburgerMenu.classList.toggle('is-active')
-    MobileNav.classList.toggle('is-active')
-    })
-})
+MobileNavLists.forEach((link) => {
+  link.addEventListener('click', () => {
+    HamburgerMenu.classList.toggle('is-active');
+    MobileNav.classList.toggle('is-active');
+  });
+});
